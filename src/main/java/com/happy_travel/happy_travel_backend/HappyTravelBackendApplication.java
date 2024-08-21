421e9492edbd09ed9744fe4b49f559c475440182
Laura Pino <lpigueroa@gmail.com>
2024-08-21 12:09:36 +0200

    Adding repo configuration

---
+++ src/main/java/com/happy_travel/happy_travel_backend/HappyTravelBackendApplication.java
@@ -0,0 +1,15 @@
+package com.happy_travel.happy_travel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTravelBackendApplication.class, args);
+
+		System.out.println("Hola!");
+	}
+
+}
---
+++ src/test/java/com/happy_travel/happy_travel_backend/HappyTravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy_travel.happy_travel_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
