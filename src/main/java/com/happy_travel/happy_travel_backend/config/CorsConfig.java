5486b80371ea150513e98c223cdf32f52f480b41
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-08-22 06:30:02 -0600

    adding beans

--- src/main/java/com/happy_travel/happy_travel_backend/config/CorsConfig.java
+++ src/main/java/com/happy_travel/happy_travel_backend/config/CorsConfig.java
@@ -1,19 +1,19 @@
 package com.happy_travel.happy_travel_backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
+@Configuration
 public class CorsConfig {
     
     // Bean para la configuración de CORS
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5174"));
-
-        // Cambia a la URL de tu frontend
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
